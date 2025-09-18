1c0981f4648a28af4042265080776a8f97d9ddcd
Richard Rhodes <rgrhodesdev@gmail.com>
2025-09-18 22:59:08 +0100

    added format check back in

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    description = "The region for the AWS account"
-    type        = string
+  description = "The region for the AWS account"
+  type        = string
 }
 
 variable "env" {
-    description = "The deployment environment"
-    type        = string
+  description = "The deployment environment"
+  type        = string
 }
