b8b99f1ecd01132f74cfddbfe911a90d2836d72e
Richard Rhodes <rgrhodesdev@gmail.com>
2025-09-18 21:27:01 +0100

    Initial piepline setup

---
+++ scripts/tf-prepare-environment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+REPO_TOP_LEVEL="$(git rev-parse --show-toplevel)"
+pushd "${REPO_TOP_LEVEL}"
+trap 'popd' ERR
+
+cd ./iac-scripts/terraform
+rm ./*.tfvars || true
+rm ./*.tfvars.json || true
+rm ./import.tf || true
+rm backend.conf || true
+cp ./environments/"$1"/* ./
+
+popd
