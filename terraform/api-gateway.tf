e563ff3026762952a65522d3f8e7b4ba5dcf119a
Richard Rhodes <rgrhodesdev@gmail.com>
2025-09-20 16:28:30 +0100

    fix

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -11,9 +11,6 @@ resource "aws_api_gateway_rest_api" "utils" {
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration
-  ]
 
   rest_api_id = aws_api_gateway_rest_api.utils.id
 
