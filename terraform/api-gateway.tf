1d6e347ffa11ea443e7a6727ed00f389b041fbe8
Richard Rhodes <rgrhodesdev@gmail.com>
2025-09-21 17:49:49 +0100

    include initial code

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -8,6 +8,12 @@ resource "aws_api_gateway_rest_api" "utils" {
 
   }
 
+  endpoint_configuration {
+
+    types = ["REGIONAL"]
+
+  }
+
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,3 @@ variable "env" {
   description = "The deployment environment"
   type        = string
 }
-
-variable "http_method" {
-  description = "The HTTP Method"
-  type        = string
-}
-
-variable "path_part" {
-  description = "The vlaue of the API path under the root resource"
-  type        = string
-}
