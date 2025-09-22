c9840d212bfbd85801093cbd2527c7ac125fe1a7
Richard Rhodes <rgrhodesdev@gmail.com>
2025-09-22 10:27:02 +0100

    Added checkov

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,8 +1,8 @@
 resource "aws_dynamodb_table" "s3_audit_table" {
-  name             = "${var.env}_s3_audit_table"
-  billing_mode     = "PROVISIONED"
-  read_capacity    = 1
-  write_capacity   = 1
+  name           = "${var.env}_s3_audit_table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
   hash_key       = "BucketName"
 
   attribute {
